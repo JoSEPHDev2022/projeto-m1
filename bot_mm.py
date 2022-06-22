ba2f61132af375f0d3b53e273dc48f80f7034ce4
José Luiz <jl_ferreira16@hotmail.com>
2022-06-22 14:20:20 -0300

    Pequenas modificações na visualização e design da saída de dados

--- bot_mm.py
+++ bot_mm.py
@@ -1,13 +1,14 @@
 import setores_financeiros as set_if
 
 def inicio():
-    print(' Sou o Assistente Virtual da M&M.\n Conte comigo para conhecer os recursos do Portal.\n Para começar, selecione um dos itens abaixo para que eu possa te ajudar!')
-    print('1) Empréstimo')
+    print('Sou o Assistente Virtual da M&M, conte comigo para conhecer os recursos do Portal.'.center(130, '='))
+    print('Para começar, selecione um dos itens abaixo para que eu possa te ajudar!'.center(130, '='))
+    print('\n1) Empréstimo')
     print('2) Financiamento')
     print('3) Benefícios')
     print('4) Fale Conosco')
     print('5) Sair do programa')
-    txt = int(input('Digite a opção desejada:'))
+    txt = int(input('\nDigite a opção desejada: '))
     if txt == 1:
         set_if.emprestimo()
     if txt == 2:
@@ -17,6 +18,7 @@ def inicio():
     if txt == 4:
         set_if.fale_conosco()
     if txt == 5:
+        print('Foi um prazer ajudar!')
         exit()
     else:
         inicio()
